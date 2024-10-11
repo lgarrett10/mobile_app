1ba52ad24327124aa0809a8a0f9bc5a51994a921
ishmam <ishmam.a.solaiman@gmail.com>
2024-10-11 03:34:49 -0500

    add navigation to test screen

--- frontend/Components/TestScreen.js
+++ frontend/Components/TestScreen.js
@@ -1,23 +1,28 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, Text, StyleSheet, Button } from "react-native";
 
-export default function TestScreen() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.headerText}>Welcome to test screen!</Text>
-        </SafeAreaView>
-    );
+export default function TestScreen({ navigation }) {
+  const navigateToWelcome = () => {
+    navigation.navigate("Welcome");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerText}>Welcome to test screen!</Text>
+      <Button title="Go Back to Welcome Screen" onPress={navigateToWelcome} />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        padding: 16,
-    },
-    headerText: {
-        fontSize: 24,
-        marginBottom: 20,
-        textAlign: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16,
+  },
+  headerText: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: "center",
+  },
 });
