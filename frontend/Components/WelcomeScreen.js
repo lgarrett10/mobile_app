227ecdb223590f6c6a288877fce382ffedd5ab1e
ishmam <ishmam.a.solaiman@gmail.com>
2024-10-14 02:54:38 -0500

    add an api call to JSONPLACEHOLDER and display in FlatList

--- frontend/Components/WelcomeScreen.js
+++ frontend/Components/WelcomeScreen.js
@@ -1,6 +1,6 @@
 // WelcomeScreen.js
-import React from 'react';
-import { SafeAreaView, Text, Button, StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, Button, StyleSheet, View, FlatList, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function WelcomeScreen({ navigation, setIsLoggedIn }) {
@@ -13,13 +13,43 @@ export default function WelcomeScreen({ navigation, setIsLoggedIn }) {
     });
   };
 
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const navigateToTestScreen = () => {
     navigation.navigate('TestScreen');
   };
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=2')
+      .then((response) => response.json())
+      .then((json) =>{ 
+        setData(json)
+    })
+      .catch((error) => {
+        console.error('error fetching data', error)
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.headerText}>Welcome!</Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text>{item.body}</Text>
+            </View>
+          )}
+        />
+      )}
+
       <View style={styles.buttonContainer}>
         <Button style={styles.button} title="Test Screen" onPress={navigateToTestScreen} />
         <Button style={styles.button} title="Logout" onPress={handleLogout} />
@@ -46,5 +76,14 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'space-between',
         height: 80,
+      },
+      item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },  
+      title: {
+        fontSize: 32,
       },
 });
