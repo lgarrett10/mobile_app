c7065ddf9d4f318d1f933da0266580096abfe829
Luke Garrett <lgarrett10@atu.edu>
2024-10-31 16:31:28 -0500

    update

--- frontend/Components/LoginScreen.js
+++ frontend/Components/LoginScreen.js
@@ -25,8 +25,8 @@ export default function LoginScreen({ navigation, setIsLoggedIn }) {
     }
   
     const url = isLogin
-      ? 'http://192.168.0.8:5000/users/login'
-      : 'http://192.168.0.8:5000/users/register';
+      ? 'http://192.168.1.254:5000/users/login'
+      : 'http://192.168.1.254:5000/users/register';
   
     const body = isLogin
       ? { username: email, password }
